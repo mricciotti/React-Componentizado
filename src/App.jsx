6e57466b9f0a148d2728ead668bf174c5c6ccc8a
Matheus Ricciotti <matheus.ricciotti@gmail.com>
2024-08-23 22:50:47 -0300

    introdução react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Header from './Componentes/Header';
 import ConteudoPrincipal from './Componentes/ConteudoPrincipal';
 import Footer from './Componentes/Footer'; 
-
+import logoVite from './assests/vite.svg'
+import { Outlet } from 'react-router-dom';
 
 function App() {
  
   return (
     <>
       <Header />
-      <ConteudoPrincipal/>
+      <Outlet/>
       <Footer />
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Sobre from './pages/Sobre.jsx'
+import ConteudoPrincipal from './Componentes/ConteudoPrincipal/index.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {index: true, element: <ConteudoPrincipal/>},
+      {path: "/sobre", element: <Sobre/>}
+    ]
+  }
+])
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
